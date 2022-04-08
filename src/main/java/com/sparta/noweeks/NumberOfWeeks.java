1d0c40c74dbfe1db3de9a7c8b6ee3e960314198e
pcallum <callumrp10@gmail.com>
2022-04-08 16:10:59 +0100

    filled in classes

--- src/main/java/com/sparta/noweeks/NumberOfWeeks.java
+++ src/main/java/com/sparta/noweeks/NumberOfWeeks.java
@@ -2,9 +2,27 @@ package com.sparta.noweeks;
 
 public class NumberOfWeeks {
     public static String getNoWeeks(int days) {
-        String noWeeks = "x weeks y days";
-        
+        int weeks = Math.floorDiv(days, 7);
+        days = days % 7;
 
-        return noWeeks;
+        StringBuilder str = new StringBuilder();
+
+        str.append(weeks);
+
+        if (weeks == 1) {
+            str.append(" week and ");
+        } else {
+            str.append(" weeks and ");
+        }
+
+        str.append(days);
+
+        if (days == 1) {
+            str.append(" day");
+        } else {
+            str.append(" days");
+        }
+
+        return str.toString();
     }
 }
--- src/main/java/com/sparta/noweeks/NumberOfWeeksDriver.java
+++ src/main/java/com/sparta/noweeks/NumberOfWeeksDriver.java
@@ -2,6 +2,7 @@ package com.sparta.noweeks;
 
 public class NumberOfWeeksDriver {
     public static void main(String[] args) {
-
+        NumberOfWeeks noWeeks = new NumberOfWeeks();
+        System.out.println(noWeeks.getNoWeeks(43));
     }
 }
